3c45ebf6145613fe191db6ecc5fea6ccbd477751
TmothyMartel <t.m.martel@gmail.com>
2020-01-26 15:12:10 -0500

    final styling added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import "./App.css";
 
 //class gives access to and alllows us to change state
@@ -9,8 +11,11 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,10 +24,24 @@ class App extends Component {
       .then(users => this.setState({monsters: users}))
   }
 
+  //arrow fuctions automaticallyget lexical scoping, meaning it binds the "this" context
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
